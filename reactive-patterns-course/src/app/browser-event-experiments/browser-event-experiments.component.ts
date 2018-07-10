7a09d76af8a985ae0bea0b72161caa20b5b17fae
Aiden Campbell <campbell.e.aiden@gmail.com>
2018-07-10 23:56:49 +0200

    rxjs

--- reactive-patterns-course/src/app/browser-event-experiments/browser-event-experiments.component.ts
+++ reactive-patterns-course/src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browser-event-experiments.component.css']
 })
 export class BrowserEventExperimentsComponent implements OnInit {
-
+  hoverSection: HTMLElement;
   constructor() { }
 
   ngOnInit() {
+    this.hoverSection = document.getElementById('hover');
+
+    this.hoverSection.addEventListener(`mousemove`, onMouseMove );
   }
 
+  unsubscribe() {
+    console.log(`Called Unsubscribe()`);
+    this.hoverSection.removeEventListener(`mousemove`, onMouseMove );
+  }
+
+  subscribe() {
+    console.log(`Called Subscribe()`);
+    this.hoverSection.addEventListener(`mousemove`, onMouseMove );
+  }
+}
+
+function onMouseMove(ev) {
+  console.log(ev);
 }
